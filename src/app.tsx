446878d9b4e76bd2c9fe5ceae4b8d7a325c126db
IlyaMuralha <muralhakuzmenko@gmail.com>
2025-06-26 21:25:00 +0500

    Добавляет и подключает компонент PrivateRoute

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,15 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
-import { AppRoute } from './const';
+import { AppRoute, AuthorizationStatus } from './const';
 import MainScreen from './pages/main-screen/main-screen';
 import LoginScreen from './pages/login-screen/login-screen';
 import FavoritesScreen from './pages/favorites-screen/favorites-screen';
 import OfferScreen from './pages/offer-screen/offer-screen';
 import NotFoundScreen from './pages/not-found-screen/not-found-screen';
-
+import PrivateRoute from './components/private-route/private-route';
 
 type AppScreenProps = {
   offerCardCount: number;
 }
-
 
 function App({offerCardCount}: AppScreenProps): JSX.Element {
   return (
@@ -18,7 +17,14 @@ function App({offerCardCount}: AppScreenProps): JSX.Element {
       <Routes>
         <Route path={AppRoute.Main} element={<MainScreen offerCardCount={offerCardCount}/>} />
         <Route path={AppRoute.Login} element={<LoginScreen/>} />
-        <Route path={AppRoute.Favorites} element={<FavoritesScreen/>} />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen/>
+            </PrivateRoute>
+          }
+        />
         <Route path={AppRoute.Offer} element={<OfferScreen/>} />
         <Route path='*' element={<NotFoundScreen/>} />
       </Routes>
