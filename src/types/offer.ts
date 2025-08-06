4584ba10803e12890b56c71b86e0f18c0db2833c
IlyaMuralha <muralhakuzmenko@gmail.com>
2025-08-07 00:24:51 +0500

    Создает моковые данные

---
+++ src/types/offer.ts
@@ -0,0 +1,34 @@
+export type THost = {
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+}
+
+export type TLocationCoordinates = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+export type TCity = {
+  name: string;
+  location: TLocationCoordinates;
+}
+
+export type TOffer = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  city: TCity;
+  location: TLocationCoordinates;
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+  description: string;
+  bedrooms: number;
+  goods: string[];
+  host: THost;
+  images: string[];
+  maxAdults: number;
+}
